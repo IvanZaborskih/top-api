07869c078ca55a0c1b6fd5e19cc27394267c406f
Ivan <34769715+IvanZaborskih@users.noreply.github.com>
2023-07-24 14:35:04 +0300

    4 - split on modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { ReviewModule } from './modules/review/review.module';
+import { TopPageModule } from './modules/top-page/top-page.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ReviewModule, TopPageModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
