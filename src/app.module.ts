96b8aaa8537ed976f378724ca58cf0b5c68a498a
Ivan <34769715+IvanZaborskih@users.noreply.github.com>
2023-10-21 12:55:00 +0300

    4 - create controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
-import { ReviewModule } from './modules/review/review.module';
-import { TopPageModule } from './modules/top-page/top-page.module';
-import { ProductModule } from './modules/product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { AuthController } from './auth/auth.controller';
+import { ProductController } from './product/product.controller';
+import { ReviewController } from './review/review.controller';
+import { TopPageController } from './top-page/top-page.controller';
 
 @Module({
   imports: [AuthModule, ReviewModule, TopPageModule, ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AuthController, ProductController, ReviewController, TopPageController],
+  providers: [],
 })
 export class AppModule {}
