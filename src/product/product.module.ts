c3af06010aea0196c74efb0f1036330e55467d35
Ivan <34769715+IvanZaborskih@users.noreply.github.com>
2023-10-25 21:34:25 +0300

    5 - connect models

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { ProductModel, ProductSchema } from './product.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [ProductController],
+  imports: [
+    MongooseModule.forFeature([
+      { name: ProductModel.name, schema: ProductSchema },
+    ]),
+  ],
 })
 export class ProductModule {}
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,8 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type TopPageDocument = HydratedDocument<TopPageModel>;
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,23 +10,66 @@ export enum TopLevelCategory {
   Products,
 }
 
-export class TopPageModel {
-  id: string;
-  firstCategory: TopLevelCategory;
-  secondCategory: string;
+@Schema()
+export class HhData {
+  @Prop()
+  count: number;
+
+  @Prop()
+  juniorSalary: number;
+
+  @Prop()
+  middleSalary: number;
+
+  @Prop()
+  seniorSalary: number;
+}
+
+export const HhDataSchema = SchemaFactory.createForClass(HhData);
+
+@Schema()
+export class TopPageAdvantage {
+  @Prop()
   title: string;
+
+  @Prop()
+  description: string;
+}
+
+export const TopPageAdvantageSchema =
+  SchemaFactory.createForClass(TopPageAdvantage);
+
+@Schema({ timestamps: true })
+export class TopPageModel {
+  @Prop({ enum: TopLevelCategory })
+  firstCategory: TopLevelCategory;
+
+  @Prop()
+  secondCategory: string;
+
+  @Prop({ unique: true })
+  alias: string;
+
+  @Prop()
+  title: string;
+
+  @Prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @Prop({ type: HhDataSchema })
+  hh?: HhData;
+
+  @Prop({ type: [TopPageAdvantageSchema] })
+  advantages: TopPageAdvantage[];
+
+  @Prop()
   seoText: string;
+
+  @Prop()
   tagsTitle: string;
+
+  @Prop({ type: [String] })
   tags: string[];
 }
+
+export const TopPageSchema = SchemaFactory.createForClass(TopPageModel);
