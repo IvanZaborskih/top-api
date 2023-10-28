6934d68de6502eb868989df458e95d62d21f19c3
Ivan <34769715+IvanZaborskih@users.noreply.github.com>
2023-10-28 16:15:56 +0300

    7 - validation pipe

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,6 +7,8 @@ import {
   HttpStatus,
   Param,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
@@ -17,6 +19,7 @@ import { Review } from './review.model';
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post()
   async create(@Body() dto: CreateReviewDto): Promise<Review> {
     return this.reviewService.create(dto);
